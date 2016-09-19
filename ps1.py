629fd2f6f651e1d94c9f444904916f3b85b91512
2016JTM2087 <send.anupamjha@gmail.com>
2016-09-19 10:12:30 +0530

    Modified PS1.py

--- ps1.py
+++ ps1.py
@@ -1,3 +1,21 @@
+#!bin/python
+
 ###### this is the first .py file ###########
 
-####### write your code here ##########
+import sys				#for command line input
+
+m={}
+g=list(sys.argv)
+l=range(1,len(g)) 			# to check the range of i
+for i in range(1,len(g)):		#c=count
+	c=0				#initialise the variable	
+	for j in range(1,len(g)):	#start the sub loop	
+		if g[i]==g[j]:		#check equality condition
+			c=c+1		#increment the variable
+	l[i-1]=c	
+	print "%s occurs %d times in the string" % (g[i], c)
+	m.update({g[i]:c})		# update with the new value
+ 
+
+
+
